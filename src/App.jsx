5fc203c3206df9364196d0f9461bcc23f8328ff2
0xsamarth <0xsamarth@gmail.com>
2025-06-04 18:39:04 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,24 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Services from './pages/Services';
 import About from './pages/About';
 import Resume from './pages/Resume';
 import SearchResults from './pages/SearchResults';
 
-
-{/* Articles */}
-
+/* Articles */
 import Articles from './pages/Articles';
 import ArticleOne from "./pages/articles/article-one";
 import Nginx from "./pages/articles/nginx";
-import Linux from "./pages/articles/linux-cheatsheet"
+import Linux from "./pages/articles/linux-cheatsheet";
 import Authentication from './pages/articles/authentication';
 import AccessControl1 from './pages/articles/access-control-1';
 
-{/* Tools */}
+/* Tools */
 import Tools from './pages/Tools';
 import JSBeautifier from "./tools/beautifier";
 import GitHubFinder from './tools/git';
 
-
-
-{/* Resources */}
+/* Resources */
 import Resources from "./pages/Resources";
 import Notes from "./pages/resources/Notes";
 import Cheatsheets from "./pages/resources/Cheatsheets";
@@ -31,64 +27,39 @@ import AuthNotes from "./pages/resources/notes/AuthNotes";
 import LinuxFundamentals from './pages/resources/cheatsheets/Linux';
 import XSSPayloads from "./pages/resources/payloads/XSSPayloads";
 
-// import NginxCheat from "./pages/resources/cheatsheets/NginxCheat";
-// import BashCheat from "./pages/resources/cheatsheets/BashCheat";
-// import SQLiPayloads from "./pages/resources/payloads/SQLiPayloads";
-
-
-
-
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/resume" element={<Resume />} />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/services" element={<Services />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/resume" element={<Resume />} />
+      <Route path="/search" element={<SearchResults />} />
 
-<Route path="/search" element={<SearchResults />} />
+      {/* Articles */}
+      <Route path="/articles" element={<Articles />} />
+      <Route path="/articles/article-one" element={<ArticleOne />} />
+      <Route path="/articles/nginx" element={<Nginx />} />
+      <Route path="/articles/access-control-1" element={<AccessControl1 />} />
+      <Route path="/articles/linux-cheatsheet" element={<Linux />} />
+      <Route path="/articles/authentication" element={<Authentication />} />
 
-
-        <Route path="/articles" element={<Articles />} />
-        <Route path="/articles/article-one" element={<ArticleOne />} />
-        <Route path="/articles/nginx" element={<Nginx />} />
-        <Route path="/articles/access-control-1" element={<AccessControl1 />} />
-
-        <Route path="/articles/linux-cheatsheet" element={<Linux />} />
-        <Route path="/articles/authentication" element={<Authentication />} />  
-
-
-        {/* Tools */}
-        <Route path="/tools" element={<Tools />} />  
-
-      <Route path="tools/beautifier" element={<JSBeautifier />} />
+      {/* Tools */}
+      <Route path="/tools" element={<Tools />} />
+      <Route path="/tools/beautifier" element={<JSBeautifier />} />
       <Route path="/tools/git" element={<GitHubFinder />} />
 
-
-  
-        {/* Add more routes as needed */}
+      {/* Resources */}
       <Route path="/resources" element={<Resources />} />
       <Route path="/resources/notes" element={<Notes />} />
       <Route path="/resources/cheatsheets" element={<Cheatsheets />} />
       <Route path="/resources/payloads" element={<Payloads />} />
-      
-      {/* Nested Resource Pages */}
-     <Route path="/resources/notes/auth" element={<AuthNotes />} />
-     <Route path="/resources/payloads/xss" element={<XSSPayloads />} />
-
+      <Route path="/resources/notes/auth" element={<AuthNotes />} />
+      <Route path="/resources/payloads/xss" element={<XSSPayloads />} />
       <Route path="/resources/cheatsheets/linux" element={<LinuxFundamentals />} />
-
-  {/*
-      <Route path="/resources/notes/linux" element={<LinuxNotes />} />
-      <Route path="/resources/cheatsheets/nginx" element={<NginxCheat />} />
-      <Route path="/resources/cheatsheets/bash" element={<BashCheat />} />
-      <Route path="/resources/payloads/sqli" element={<SQLiPayloads />} /> */}
-
-
-      </Routes>
-    </Router>
+    </Routes>
   );
 }
 
 export default App;
+
